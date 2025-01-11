41de1ca7aadac7d31cd9002b078dba5c95e98f98
Dawson Yi Xiong <dawsonxiong@gmail.com>
2025-01-11 16:23:21 -0500

    Update read_receipt.py

--- read_receipt.py
+++ read_receipt.py
@@ -1,9 +1,12 @@
 import pytesseract
 from PIL import Image
 
-# Load image
-img = Image.open('test/receipt_1.jpeg') # test image
+def read(path):
+    # Load image
+    img = Image.open(path)
 
-# OCR read
-text = pytesseract.image_to_string(img, config='--psm 6')  # Suitable for single-column text
-print(text)
+    # OCR read
+    text = pytesseract.image_to_string(img, config='--psm 6')  # Suitable for single-column text
+    return
+
+test_path = 'test/receipt_1.jpeg' # test image path
